2005d8f644744aa2ab8256400211334703cd0e15
popieyes <menegom@icloud.com>
2026-02-09 19:46:36 +0100

    Updated 3D models path

--- src/components/SceneComponents/Models.jsx
+++ src/components/SceneComponents/Models.jsx
@@ -7,13 +7,13 @@ export function Office(){
             <Gltf 
             castShadow 
             receiveShadow 
-            src="/models/office-mod.glb" 
+            src={process.env.PUBLIC_URL + "/models/office-mod.glb"} 
             position={[-0.5, 0, -1.2]} />
            
             <Gltf 
             castShadow 
             receiveShadow 
-            src='/models/xbox.glb'
+            src={process.env.PUBLIC_URL + '/models/xbox.glb'}
             position={[-0.36, 0.75, -0.15]}
             rotation={[0, -1, 0]}
             scale={0.15} />
@@ -22,7 +22,7 @@ export function Office(){
             <Gltf
             castShadow
             receiveShadow
-            src='/models/canon-hfix.glb'
+            src={process.env.PUBLIC_URL + '/models/canon-hfix.glb'}
             position={[-0.13, 0.755, -0.14]}
             rotation={[-1.6,0,2]}
             scale={0.8}
@@ -31,7 +31,7 @@ export function Office(){
             <Gltf
             castShadow
             receiveShadow
-            src='/models/shiba-h.glb'
+            src={process.env.PUBLIC_URL + '/models/shiba-h.glb'}
             position={[-0.8, 0.3, 0.5]}
             rotation={[0,-5,0]}
             scale={0.3}
